a8b2636e1a115c8b574cca4c9f56911c9d8e882a
wblankenship <william.jblankenship@gmail.com>
2016-08-22 20:44:22 -0400

    Update build output

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ docker run -it -v $PWD:/foobar retrohacker/nodejs:build mv output foobar/output
 sudo chown -R wblankenship:wblankenship output
 cp ./bootstrap ./output/bootstrap
 docker build -t retrohacker/nodejs:mini -f Dockerfile.scratch .
-echo "run: docker run -it nodesource/nodejs:scratch"
+echo "run: docker run -it retrohacker/nodejs:scratch"
