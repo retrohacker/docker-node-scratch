3d34739feed077097fa1ad6fbf58aa47833654e0
wblankenship <william.jblankenship@gmail.com>
2016-08-22 20:43:15 -0400

    Rename images to retrohacker

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -f ./Dockerfile.build -t nodesource/scratch:build .
+docker build -f ./Dockerfile.build -t retrohacker/nodejs:build .
 sudo rm -rf output
-docker run -it -v $PWD:/foobar nodesource/scratch:build mv output foobar/output
+docker run -it -v $PWD:/foobar retrohacker/nodejs:build mv output foobar/output
 sudo chown -R wblankenship:wblankenship output
-cp ./node-bootstrap ./output/node-bootstrap
-docker build -t nodesource/nodejs:scratch -f Dockerfile.scratch .
+cp ./bootstrap ./output/bootstrap
+docker build -t retrohacker/nodejs:mini -f Dockerfile.scratch .
 echo "run: docker run -it nodesource/nodejs:scratch"
